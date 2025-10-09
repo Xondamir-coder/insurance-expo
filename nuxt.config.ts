93e744013c06aa1e7694a7e394bb2cff0641de60
Xondamir <xondamirnazrullayev@gmail.com>
2025-10-09 19:34:03 +0500

    google tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-11-01',
-	devtools: { enabled: true },
-
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData:
-						'@use "~/assets/styles/abstracts/_variables.scss" as *; @use "~/assets/styles/abstracts/_mixins.scss" as *;'
-				}
-			}
-		}
-	},
-	css: [
-		'~/assets/styles/main.scss' // Replace with the path to your SCSS file
-	],
-	modules: ['nuxt-swiper', '@nuxtjs/seo']
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-VSCY8WYK39',
+          async: true
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VSCY8WYK39');
+          `,
+          type: 'text/javascript'
+        }
+      ]
+    }
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@use "~/assets/styles/abstracts/_variables.scss" as *; @use "~/assets/styles/abstracts/_mixins.scss" as *;'
+        }
+      }
+    }
+  },
+  css: ['~/assets/styles/main.scss'],
+  modules: ['nuxt-swiper', '@nuxtjs/seo']
 });
